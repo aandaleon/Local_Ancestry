67009d6703f10132ad68ec617a192aa058246d18
Angela Andaleon <angie.andaleon@gmail.com>
2019-04-16 09:45:26 -0500

    try bigger window size; if not, LD prune before haps

--- 03a1_make_LAMP-LD_input.sh
+++ 03a1_make_LAMP-LD_input.sh
@@ -21,6 +21,13 @@ python ../../03a2_bcftools_haps_to_LAMP-LD_haps.py --hap.gz admixed.haps.hap.gz 
 python ../../03a2_bcftools_haps_to_LAMP-LD_haps.py --hap.gz YRI.haps.hap.gz --haps YRI.haps
 python ../../03a2_bcftools_haps_to_LAMP-LD_haps.py --hap.gz CEU.haps.hap.gz --haps CEU.haps
 
-../../LAMPLD-v1.0/bin/unolanc2way 300 15 ../chr22.pos CEU.haps YRI.haps admixed.haps admixed_est.txt #version of LAMP-LD for 2-way admixture
+#../../LAMPLD-v1.0/bin/unolanc2way 300 15 ../chr22.pos CEU.haps YRI.haps admixed.haps admixed_est.txt #version of LAMP-LD for 2-way admixture
  #use default window size of 300 and default number of states HMM 15
  #note: check the output of this morning of 4/16/19 - /home/angela/BIOI500_Local_Ancestry/sim_LAMPLD/80_20_6/admixed_est.txt
+ #ran into an error: computing at window 169 [50700 50999]; ERROR forward_hap hap[50951]=; not found
+nohup ../../LAMPLD-v1.0/bin/unolanc2way 3000 15 ../chr22.pos CEU.haps YRI.haps admixed.haps admixed_est.txt > run_04-16-19.txt
+  #trying a bigger window size
+ 
+ 
+ 
+ 
