738cb9fc31be89cc0079836f6f1c53da98f83b89
Angela Andaleon <angie.andaleon@gmail.com>
2019-04-16 22:56:19 -0500

    fix map

--- 02a1_simulate_admixture.sh
+++ 02a1_simulate_admixture.sh
@@ -20,7 +20,14 @@ vcf-query -l 1000G_CEU_YRI_22.pruned.vcf > 1000G_CEU_YRI_22.pruned.vcf_ids.txt
 Rscript ../02b_sample_1000G.R #outputs splits of 80 YRI/20 CEU or 50/50 to subset vcf to
 vcftools --vcf 1000G_CEU_YRI_22.pruned.vcf --out 1000G_80_20 --keep pop_codes_80_20.txt --chr 22 --recode
 vcftools --vcf 1000G_CEU_YRI_22.pruned.vcf --out 1000G_50_50 --keep pop_codes_50_50.txt --chr 22 --recode
-tail -n+2 /home/angela/1000GP_Phase3_combined/genetic_map_chr22_combined_b37.txt | awk -F' ' '{print "22",$1,$3}' | awk -v OFS="\t" '$1=$1' > genetic_map_chr22.txt #genetic map w/o "chr"
+
+#FIX GENETIC MAP 4-16-19
+wget https://github.com/joepickrell/1000-genomes-genetic-maps/raw/master/interpolated_from_hapmap/chr22.interpolated_genetic_map.gz
+gunzip chr22.interpolated_genetic_map.gz
+grep -v "^##" 1000G_CEU_YRI_22.pruned.vcf | cut -f1-3 > chr22.interpolated_genetic_map.snps.txt #pull SNPs in file
+  #give genetic positions to SNPs in file and save as map
+
+#tail -n+2 /home/angela/1000GP_Phase3_combined/genetic_map_chr22_combined_b37.txt | awk -F' ' '{print "22",$1,$3}' | awk -v OFS="\t" '$1=$1' > genetic_map_chr22.txt #genetic map w/o "chr"
 
 #four combinations of admixed pops to test
 python do-admixture-simulation.py --input-vcf 1000G_80_20.recode.vcf --sample-map pop_codes_80_20.txt --chromosome 22 --n-output 20 --n-generations 6 --genetic-map genetic_map_chr22.txt --output-basename admixed_80_20_6_gen
