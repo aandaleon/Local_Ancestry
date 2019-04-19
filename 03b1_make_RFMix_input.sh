22ed43bd9f1e68823f527fc6f94abc24465b3291
Angela Andaleon <angie.andaleon@gmail.com>
2019-04-19 12:46:44 -0500

    add running

--- 03b1_make_RFMix_input.sh
+++ 03b1_make_RFMix_input.sh
@@ -7,8 +7,7 @@ awk '{if ($2 == "CEU") {print $1}}' ../admixture-simulation/pop_codes_50_50.txt 
 awk '{if ($2 == "YRI") {print $1}}' ../admixture-simulation/pop_codes_80_20.txt > YRI_80.txt
 awk '{if ($2 == "CEU") {print $1}}' ../admixture-simulation/pop_codes_80_20.txt > CEU_20.txt
 
-#4-16-19 #INCORRECT MAP FIX THIS ASAP
-awk '{print $3}' ../admixture-simulation/genetic_map_chr22.txt > chr22.snp_locations
+#make cM map
 
 #80_20_6/
 mkdir 80_20_6/
@@ -31,3 +30,69 @@ Rscript ../../03b2_make_RFMix_classes.R merged.vcf_ids.txt
 bcftools convert --hapsample --vcf-ids merged.vcf -o merged.haps
 zcat merged.haps.hap.gz | awk '{ $1=""; $2=""; $3=""; $4=""; $5=""; print}' | sed 's/\s//g' > merged.haps #I'm impressed I could do this in one line tbh
 
+#make cM map
+sed  '/##/d' merged.vcf | cut -f3 > admixed.snps #get snps that are in admixed
+cd ../..
+Rscript 03b3_make_RFMix_genetic_map.R
+cd sim_RFMix/
+echo "$(tail -n +2 admixed_chr22.pos)" > admixed_chr22.pos
+
+#run RFMix
+cd ../RFMix_v1.5.4/ #it has to be run from the directory for some stupid reason
+python RunRFMix.py PopPhased ../sim_RFMix/80_20_6/merged.haps ../sim_RFMix/80_20_6/merged.classes ../sim_RFMix/admixed_chr22.pos -o ../sim_RFMix/80_20_6/results
+cd ../sim_RFMix/
+
+#80_20_60
+mkdir 80_20_60/
+cd 80_20_60/
+bcftools view -S ../YRI_80.txt --force-samples -Oz -o YRI_ref.vcf.gz ../../admixture-simulation/admixed_80_20_60_gen.ref.bcf.gz
+bcftools view -S ../CEU_20.txt --force-samples -Oz -o CEU_ref.vcf.gz ../../admixture-simulation/admixed_80_20_60_gen.ref.bcf.gz
+cp ../../admixture-simulation/admixed_80_20_6_gen.query.vcf admixed.vcf; bgzip admixed.vcf
+bcftools index --threads 40 -f --tbi YRI_ref.vcf.gz > YRI_ref.vcf.tbi
+bcftools index --threads 40 -f --tbi CEU_ref.vcf.gz > CEU_ref.vcf.tbi
+bcftools index --threads 40 -f --tbi admixed.vcf.gz > admixed.vcf.tbi
+bcftools merge -Ov YRI_ref.vcf.gz CEU_ref.vcf.gz admixed.vcf.gz -o merged.vcf
+vcf-query -l merged.vcf > merged.vcf_ids.txt
+Rscript ../../03b2_make_RFMix_classes.R merged.vcf_ids.txt
+bcftools convert --hapsample --vcf-ids merged.vcf -o merged.haps
+zcat merged.haps.hap.gz | awk '{ $1=""; $2=""; $3=""; $4=""; $5=""; print}' | sed 's/\s//g' > merged.haps 
+cd ../../RFMix_v1.5.4/
+python RunRFMix.py PopPhased ../sim_RFMix/80_20_60/merged.haps ../sim_RFMix/80_20_60/merged.classes ../sim_RFMix/admixed_chr22.pos -o ../sim_RFMix/80_20_60/results
+cd ../sim_RFMix/
+ 
+#50_50_6
+mkdir 50_50_6/
+cd 50_50_6/
+bcftools view -S ../YRI_80.txt --force-samples -Oz -o YRI_ref.vcf.gz ../../admixture-simulation/admixed_50_50_6_gen.ref.bcf.gz
+bcftools view -S ../CEU_20.txt --force-samples -Oz -o CEU_ref.vcf.gz ../../admixture-simulation/admixed_50_50_6_gen.ref.bcf.gz
+cp ../../admixture-simulation/admixed_80_20_6_gen.query.vcf admixed.vcf; bgzip admixed.vcf
+bcftools index --threads 40 -f --tbi YRI_ref.vcf.gz > YRI_ref.vcf.tbi
+bcftools index --threads 40 -f --tbi CEU_ref.vcf.gz > CEU_ref.vcf.tbi
+bcftools index --threads 40 -f --tbi admixed.vcf.gz > admixed.vcf.tbi
+bcftools merge -Ov YRI_ref.vcf.gz CEU_ref.vcf.gz admixed.vcf.gz -o merged.vcf
+vcf-query -l merged.vcf > merged.vcf_ids.txt
+Rscript ../../03b2_make_RFMix_classes.R merged.vcf_ids.txt
+bcftools convert --hapsample --vcf-ids merged.vcf -o merged.haps
+zcat merged.haps.hap.gz | awk '{ $1=""; $2=""; $3=""; $4=""; $5=""; print}' | sed 's/\s//g' > merged.haps 
+cd ../../RFMix_v1.5.4/
+python RunRFMix.py PopPhased ../sim_RFMix/50_50_6/merged.haps ../sim_RFMix/50_50_6/merged.classes ../sim_RFMix/admixed_chr22.pos -o ../sim_RFMix/50_50_6/results
+cd ../sim_RFMix/
+
+#50_50_60
+mkdir 50_50_60/
+cd 50_50_60/
+bcftools view -S ../YRI_80.txt --force-samples -Oz -o YRI_ref.vcf.gz ../../admixture-simulation/admixed_50_50_60_gen.ref.bcf.gz
+bcftools view -S ../CEU_20.txt --force-samples -Oz -o CEU_ref.vcf.gz ../../admixture-simulation/admixed_50_50_60_gen.ref.bcf.gz
+cp ../../admixture-simulation/admixed_80_20_6_gen.query.vcf admixed.vcf; bgzip admixed.vcf
+bcftools index --threads 40 -f --tbi YRI_ref.vcf.gz > YRI_ref.vcf.tbi
+bcftools index --threads 40 -f --tbi CEU_ref.vcf.gz > CEU_ref.vcf.tbi
+bcftools index --threads 40 -f --tbi admixed.vcf.gz > admixed.vcf.tbi
+bcftools merge -Ov YRI_ref.vcf.gz CEU_ref.vcf.gz admixed.vcf.gz -o merged.vcf
+vcf-query -l merged.vcf > merged.vcf_ids.txt
+Rscript ../../03b2_make_RFMix_classes.R merged.vcf_ids.txt
+bcftools convert --hapsample --vcf-ids merged.vcf -o merged.haps
+zcat merged.haps.hap.gz | awk '{ $1=""; $2=""; $3=""; $4=""; $5=""; print}' | sed 's/\s//g' > merged.haps 
+cd ../../RFMix_v1.5.4/
+python RunRFMix.py PopPhased ../sim_RFMix/50_50_60/merged.haps ../sim_RFMix/50_50_60/merged.classes ../sim_RFMix/admixed_chr22.pos -o ../sim_RFMix/50_50_60/results
+cd ..
+ 
