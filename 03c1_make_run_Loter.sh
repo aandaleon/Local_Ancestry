c3d8e92320345368539bd9d2461dab7836df3319
Angela Andaleon <angie.andaleon@gmail.com>
2019-04-19 12:51:52 -0500

    all runs

--- 03c1_make_run_Loter.sh
+++ 03c1_make_run_Loter.sh
@@ -11,22 +11,6 @@ python 03c2_make_Loter_npy.py
 cd Loter/
 loter_cli -r ../sim_Loter/80_20_6/YRI.npy -r ../sim_Loter/80_20_6/YRI.npy -a ../sim_Loter/80_20_6/adm.npy -o ../sim_Loter/80_20_6/results.txt
   #weird issues w/ trying to get allel installed (broke pip and now it won't work)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+loter_cli -r ../sim_Loter/80_20_60/YRI.npy -r ../sim_Loter/80_20_60/YRI.npy -a ../sim_Loter/80_20_60/adm.npy -o ../sim_Loter/80_20_60/results.txt
+loter_cli -r ../sim_Loter/50_50_6/YRI.npy -r ../sim_Loter/50_50_6/YRI.npy -a ../sim_Loter/50_50_6/adm.npy -o ../sim_Loter/50_50_6/results.txt
+loter_cli -r ../sim_Loter/50_50_60/YRI.npy -r ../sim_Loter/50_50_60/YRI.npy -a ../sim_Loter/50_50_60/adm.npy -o ../sim_Loter/50_50_60/results.txt
