69b77ab6a21c146d25eff6839bc05b8abb42c3a7
Angela Andaleon <angie.andaleon@gmail.com>
2019-04-25 10:41:44 -0500

    add benchmarking commands

--- 99_wrapper.sh
+++ 99_wrapper.sh
@@ -13,3 +13,12 @@ bash 03d_make_run_ELAI.sh
 
 echo python 04c_calc_accuracy.py
 python 04c_calc_accuracy.py
+
+echo bash 05a_make_benchmark_input.sh
+bash 05a_make_benchmark_input.sh
+
+echo qsub 05b_run_benchmarching.pbs
+qsub 05b_run_benchmarching.pbs
+
+echo bash 05c_extract_times.sh
+bash 05c_extract_times.sh
