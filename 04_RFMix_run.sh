426385c52b1c3ceaf99cf0b6a98865693100b961
Angela Andaleon <angie.andaleon@gmail.com>
2019-06-07 11:35:38 -0500

    Add files via upload

---
+++ 04_RFMix_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -N local_anc_benchmarking
+#PBS -S /bin/bash
+#PBS -l walltime=200:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=16gb 
+#PBS -o logs/${PBS_JOBNAME}.o${PBS_JOBID}.log
+#PBS -e logs/${PBS_JOBNAME}.e${PBS_JOBID}.err
+cd $PBS_O_WORKDIR
+
+#calc RFMix for 6 pops
+for pop in MXL PUR ACB ASW EVEN BRYC; do
+  echo Now running ${pop}.
+  /home/angela/anaconda2/bin/python 04_RFMix.py --ref_bcf /home/ryan/Local_ancestry/admixture-simulation/${pop}_simulation.ref.bcf.gz --query_vcf /home/ryan/Local_ancestry/admixture-simulation/${pop}_simulation.query.vcf --pop admixture_simulation/${pop}.txt
+done
