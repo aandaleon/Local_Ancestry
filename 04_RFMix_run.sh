f8f8ee1cc42e4ea881336c5ae428a79dc531a4ed
Angela Andaleon <angie.andaleon@gmail.com>
2019-06-11 09:56:25 -0500

    use LD pruned genotypes

--- 04_RFMix_run.sh
+++ 04_RFMix_run.sh
@@ -11,5 +11,5 @@ cd $PBS_O_WORKDIR
 #calc RFMix for 6 pops
 for pop in MXL PUR ACB ASW EVEN BRYC; do
   echo Now running ${pop}.
-  /home/angela/anaconda2/bin/python 04_RFMix.py --ref_bcf /home/ryan/Local_ancestry/admixture-simulation/${pop}_simulation.ref.bcf.gz --query_vcf /home/ryan/Local_ancestry/admixture-simulation/${pop}_simulation.query.vcf --pop admixture_simulation/${pop}.txt
+  /home/angela/anaconda2/bin/python 04_RFMix.py --ref_bcf /home/ryan/Local_ancestry/admixture-simulation/${pop}_simulation.ref.bcf.gz --query_vcf /home/ryan/Local_ancestry/admixture-simulation/${pop}_simulation_subset.query.recode.vcf --pop admixture_simulation/${pop}.txt
 done
